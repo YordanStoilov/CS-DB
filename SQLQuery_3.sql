794f4f17b87e298315c014dcbc39d5e65eafc8c8
YordanStoilov <iordan27@gmail.com>
2024-09-05 18:24:26 +0300

    solved task 4

--- SQLQuery_3.sql
+++ SQLQuery_3.sql
@@ -1,3 +1,9 @@
-ALTER TABLE Minions
-    ADD CONSTRAINT FK_Minions_Towns
-    FOREIGN KEY (TownId) REFERENCES Towns(Id);
+INSERT INTO Towns(Id, [Name])
+    VALUES (1, 'Sofia'), 
+           (2, 'Plovdiv'),
+           (3, 'Varna');
+
+INSERT INTO Minions(Id, Name, Age, TownId)
+    Values (1, 'Kevin', 22, 1),
+           (2, 'Bob', 15, 3),
+           (3, 'Steward', NULL, 2);

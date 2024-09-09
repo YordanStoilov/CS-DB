4b587d46ce951c1e25aa6a65bae0db7f2b755f10
YordanStoilov <iordan27@gmail.com>
2024-09-09 15:19:45 +0300

    solved task 16

---
+++ 02.CRUD/16.CreateViewEmployeesWithSalaries.sql
@@ -0,0 +1,9 @@
+-- Create a SQL query that creates a view "V_EmployeesSalaries" with first name,
+--  last name and salary for each employee.
+
+CREATE VIEW V_EmployeesSalaries AS
+SELECT
+    FirstName
+    ,LastName
+    ,Salary 
+FROM Employees;

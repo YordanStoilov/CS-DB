954be0025b017821924acbe4e8fa7073ad476cf3
YordanStoilov <iordan27@gmail.com>
2024-09-09 14:46:20 +0300

    solved task 9

---
+++ 02.CRUD/09. FindNamesOfAllEmployeesBySalaryInRange.sql
@@ -0,0 +1,9 @@
+-- Create a SQL query to find the first name, last name and job title for all employees whose 
+-- salary is in a range between 20000 and 30000.
+
+SELECT 
+    FirstName
+    ,LastName 
+    ,JobTitle 
+FROM Employees 
+WHERE Salary BETWEEN 20000 AND 30000;

a73a020a0522d82b91266486cf95a17f157657d7
YordanStoilov <iordan27@gmail.com>
2024-09-13 23:24:40 +0300

    solved task 9

--- 05.SubqueriesAndJoins/5.EmployeesWithoutProject.sql
+++ 05.SubqueriesAndJoins/5.EmployeesWithoutProject.sql
@@ -4,9 +4,10 @@
 -- Filter only employees without a project. Return the first 3 rows sorted by EmployeeID in ascending order.
 
 SELECT TOP 3
-    EmployeeID
-    ,FirstName
-FROM Employees
-WHERE EmployeeID NOT IN 
-(SELECT EmployeeID FROM EmployeesProjects)
-ORDER BY EmployeeID
+    e.EmployeeID
+    ,e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep
+ON e.EmployeeID = ep.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY e.EmployeeID

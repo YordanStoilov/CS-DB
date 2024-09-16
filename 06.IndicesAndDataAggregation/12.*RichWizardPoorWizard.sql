92dbf0b759f877c054c354f65109fad7ca512713
YordanStoilov <iordan27@gmail.com>
2024-09-16 11:18:48 +0300

    solved task 12

--- 06.IndicesAndDataAggregation/12.*RichWizardPoorWizard.sql
+++ 06.IndicesAndDataAggregation/12.*RichWizardPoorWizard.sql
@@ -3,3 +3,15 @@
 -- In the end you have to sum the difference between the deposits.
 -- At the end your query should return a single value: the SUM of all differences.
 
+WITH WizardDeposits AS 
+(
+    SELECT 
+        ID,
+        DepositAmount,
+        LEAD(DepositAmount) OVER (ORDER BY ID) AS NextDeposit
+    FROM WizzardDeposits
+)
+SELECT
+    SUM(DepositAmount - NextDeposit)
+FROM WizardDeposits
+WHERE NextDeposit IS NOT NULL

d59be59fb37cebd99752f333c74524a9bf01fef3
YordanStoilov <iordan27@gmail.com>
2024-09-17 18:01:15 +0300

    solved task 5

---
+++ 07.FunctionsAndStoredProcedures/5.SalaryLevelFunction.sql
@@ -0,0 +1,18 @@
+-- Create a function ufn_GetSalaryLevel(@salary DECIMAL(18,4)) that receives 
+-- salary of an employee and returns the level of the salary.
+-- •	If salary is < 30000, return "Low"
+-- •	If salary is between 30000 and 50000 (inclusive), return "Average"
+-- •	If salary is > 50000, return "High"
+
+CREATE OR ALTER FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(10)
+AS
+    BEGIN
+        DECLARE @Result VARCHAR(10)
+            IF @salary < 30000
+                SET @Result = 'Low'
+            ELSE IF  @salary BETWEEN 30000 AND 50000
+                SET @Result = 'Average'
+            ELSE SET @Result = 'High'
+        RETURN @Result
+    END

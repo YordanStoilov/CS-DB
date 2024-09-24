9ff912599937a0710c8bf4673a1f164c3eed3c7d
YordanStoilov <iordan27@gmail.com>
2024-09-24 12:59:58 +0300

    solved task 3

---
+++ 08.TriggersAndTransactions/3.DepositMoney.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER PROC usp_DepositMoney(@AccountId INT , @MoneyAmount DECIMAL(18, 4))
+AS
+BEGIN
+BEGIN TRANSACTION
+    IF @MoneyAmount > 0
+    BEGIN
+        UPDATE Accounts
+        SET Balance += @MoneyAmount
+        WHERE Id = @AccountId
+    END
+COMMIT TRANSACTION
+END

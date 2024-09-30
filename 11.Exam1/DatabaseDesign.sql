3da0027e1cd13949540e29eeeb6cffd4e0786f38
YordanStoilov <iordan27@gmail.com>
2024-09-30 12:29:18 +0300

    solved task 1

---
+++ 11.Exam1/DatabaseDesign.sql
@@ -0,0 +1,55 @@
+CREATE TABLE Countries
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Destinations
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    CountryId INT NOT NULL FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Rooms
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    [Type] NVARCHAR(40) NOT NULL,
+    Price DECIMAL(18, 2) NOT NULL,
+    BedCount INT NOT NULL CHECK (BedCount > 0 AND BedCount <= 10)
+)
+
+CREATE TABLE Hotels
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    DestinationId INT NOT NULL FOREIGN KEY REFERENCES Destinations(Id)
+)
+
+CREATE TABLE Tourists
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(80) NOT NULL,
+    PhoneNumber NVARCHAR(20) NOT NULL,
+    Email NVARCHAR(80),
+    CountryId INT NOT NULL FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Bookings
+(
+    Id INT NOT NULL IDENTITY PRIMARY KEY,
+    ArrivalDate DATETIME2 NOT NULL,
+    DepartureDate DATETIME2 NOT NULL,
+    AdultsCount INT NOT NULL CHECK (AdultsCount >= 1 AND AdultsCount <= 10),
+    ChildrenCount INT NOT NULL CHECK (ChildrenCount >= 0 AND ChildrenCount <= 9),
+    TouristId INT NOT NULL FOREIGN KEY REFERENCES Tourists(Id),
+    HotelId INT NOT NULL FOREIGN KEY REFERENCES Hotels(Id),
+    RoomId INT NOT NULL FOREIGN KEY REFERENCES Rooms(Id)
+)
+
+CREATE TABLE HotelsRooms
+(
+    HotelId INT NOT NULL FOREIGN KEY REFERENCES Hotels(Id),
+    RoomId INT NOT NULL FOREIGN KEY REFERENCES Rooms(Id)
+    PRIMARY KEY (HotelId, RoomId)
+)

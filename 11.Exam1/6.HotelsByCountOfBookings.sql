346a63aa0a75c9b7d68598323a57c65dce0f774e
YordanStoilov <iordan27@gmail.com>
2024-09-30 16:13:35 +0300

    solved task 6

--- 11.Exam1/6.HotelsByCountOfBookings.sql
+++ 11.Exam1/6.HotelsByCountOfBookings.sql
@@ -5,12 +5,14 @@ SELECT
 FROM
     Hotels h
 JOIN    
-    HotelsRooms hr ON h.Id = hr.HotelId
+    HotelsRooms hr ON h.Id = hr.HotelId AND hr.RoomId = 8
 JOIN
     Rooms r ON r.ID = hr.RoomId
 JOIN
     Bookings b ON b.HotelId = h.Id
 GROUP BY
     h.Id, h.Name, r.Type
-HAVING r.Type = 'VIP Apartment' --The problem is that we are filtering by VIP Apartment before ordering
-ORDER BY COUNT(b.HotelId) DESC
+ORDER BY
+    COUNT(b.HotelId) DESC
+
+-- Vip apartment Id = 8
